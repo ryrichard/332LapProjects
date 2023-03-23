b2cec935d716bebc389766ddbf40d3d323631de0
ryrichard <ryrichard83@gmail.com>
2023-03-23 00:27:19 -0400

    Finished Part 4

---
+++ Project4/Part4A.c
@@ -0,0 +1,55 @@
+/*
+Write a program where a 3 process are attached to a shared memory.
+Process A, writes 5 characters {A,A,A,A,A} to the shared memory. Then waits till the 1 st
+character is changed to B by Process B , print out the new contents of the shared memory and
+then terminates.
+Process B changes the first character to B => {B,A,A,A,A} and Then waits till the 1st Character is
+changed to C by Process C, print out the new contents and then terminates.
+Process C changes the first character to C => {C,A,A,A,A} and Then detaches the shared memory
+and terminates.
+There is no parent-child relationship between the processes.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <sys/types.h>
+
+int main()
+{
+	key_t key = 9999;
+	size_t size = 100;
+	char *addr, *shm;
+	int smid;
+
+	if((smid = shmget(key, size, IPC_CREAT | 0666)) == -1) //creates the mem space and id
+	{
+		perror("shmget");
+		exit(1);
+	}	
+	else
+		printf("SMID: %d\n", smid);
+ 
+	if((addr = shmat(smid, NULL, 0)) == (char *)-1) //pointer to space 
+	{
+		perror("shmat");
+		exit(1);
+	}	
+
+	shm = addr; //data to be shared
+
+	for(int i = 0; i < 5; i++) 
+		*shm++ = 'A';
+	shm = addr; //reset shm to point to the beginning to check first char
+	
+	while(*shm != 'B') //waiting for shared memory to change
+		sleep(1);
+
+	printf("SHM: %s\n", shm);
+
+	exit(0);
+
+	return 0;
+}
